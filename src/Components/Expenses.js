210a56e1b825faf7f66abdb6ab779372b367a059
Ritik Raj <ritikraj2629@gmail.com>
2021-12-13 23:07:08 +0530

    Initialize Project Using Create React App

---
+++ src/Components/Expenses.js
@@ -0,0 +1,33 @@
+import Card from "./Card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+import { useState } from "react";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
+    console.log("Expenses.js");
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses}/>
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
